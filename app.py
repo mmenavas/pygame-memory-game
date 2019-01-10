53fe9f633431c0c8f247e91cef5f89f7d0f3639e
Maximo Mena <maximo.mena@earlywarning.com>
2019-01-09 23:21:31 -0700

    Save work

--- app.py
+++ app.py
@@ -6,19 +6,18 @@ from Game import Cards
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, text, size, color, bg_color, width, height, border, border_color):
-        # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
         self.font = pygame.font.SysFont("Arial", size)
         self.textSurf = self.font.render(text, 1, color)
         self.surf = pygame.Surface((width, height))
         self.surf.fill(border_color)
-        self.surf_inner = pygame.Surface((width - border * 2, height - border * 2))
-        self.surf_inner.fill(bg_color)
+        self.image = pygame.Surface((width - border * 2, height - border * 2))
+        self.image.fill(bg_color)
         w = self.textSurf.get_width()
         h = self.textSurf.get_height()
-        self.surf_inner.blit(self.textSurf, [width/2 - w/2, height/2 - h/2])
-        self.surf.blit(self.surf_inner, [border, border])
-
+        self.rect = self.image.get_rect()
+        self.image.blit(self.textSurf, [width/2 - w/2, height/2 - h/2])
+        self.surf.blit(self.image, [border, border])
 
 class Settings:
     def __init__(self):
@@ -46,30 +45,26 @@ pygame.init()
 
 # create the screen object
 screen = pygame.display.set_mode(settings.get_dimensions())
-
-# Add background color
-bg = pygame.Surface(settings.get_dimensions())
-bg.fill(settings.bg_color)
-screen.blit(bg, (0, 0))
+screen.fill(settings.bg_color)
 
 # Build tiles
-cards = Cards.Cards(settings.unique_tiles)
-tiles = []
-for index, card in cards.get_cards().items():
-    tiles.append(Tile(
-        str(card.get_value()),
-        settings.tile_font_size,
-        settings.tile_font_color,
-        settings.tile_bg_color,
-        settings.tile_width,
-        settings.tile_height,
-        settings.tile_border,
-        settings.tile_border_color
-    ))
+# cards = Cards.Cards(settings.unique_tiles)
+# tiles = pygame.sprite.Group()
+# for index, card in cards.get_cards().items():
+#     tiles.add(Tile(
+#         str(card.get_value()),
+#         settings.tile_font_size,
+#         settings.tile_font_color,
+#         settings.tile_bg_color,
+#         settings.tile_width,
+#         settings.tile_height,
+#         settings.tile_border,
+#         settings.tile_border_color
+#     ))
 
-facedown_tiles = []
-for tile in tiles:
-    facedown_tiles.append(Tile(
+tiles = pygame.sprite.LayeredUpdates()
+for i in range(settings.unique_tiles * 2):
+    tiles.add(Tile(
         "?",
         settings.tile_font_size,
         settings.tile_font_color,
@@ -85,6 +80,7 @@ running = True
 
 # Our main loop!
 while running:
+
     # for loop through the event queue
     for event in pygame.event.get():
         # Check for KEYDOWN event; KEYDOWN is a constant defined in pygame.locals, which we imported earlier
@@ -103,7 +99,7 @@ while running:
     while y < settings.grid_y:
         while x < settings.grid_x:
             coordinates = (x * settings.tile_width, y * settings.tile_height)
-            screen.blit(facedown_tiles[index].surf, coordinates)
+            screen.blit(tiles.get_sprite(index).surf, coordinates)
             x += 1
             index += 1
         x = 0
@@ -112,6 +108,20 @@ while running:
     # Show a single tile
     # coordinates = ((15 % settings.grid_x) * settings.tile_width, (15 % settings.grid_y) * settings.tile_height)
     # screen.blit(tiles[15].surf, coordinates)
+
+    # Get mouse data
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    # Handle mouse click
+    # if item.image.get_rect().collidepoint(mouse):
+    items = len(tiles.get_sprites_at(mouse))
+    print(items)
+    # if items:
+    #     if click[0] == 1:
+    #         print("Tile {}".format(items))
+    # else:
+    #     pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
 
     # # Update the display
     pygame.display.flip()
